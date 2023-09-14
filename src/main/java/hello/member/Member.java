b6a0ad82f4a7cd9437c3d2929180a73938f02eb2
ChickenchickenLove <ojt90902@naver.com>
2023-09-14 23:59:22 +0900

    lecture1

---
+++ src/main/java/hello/member/Member.java
@@ -0,0 +1,15 @@
+package hello.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+    private String memberId;
+    private String name;
+
+}
